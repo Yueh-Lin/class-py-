0e4564a5c7a42af1eabfde0f9cf029331ee78697
Yueh-Lin <86942074+Yueh-Lin@users.noreply.github.com>
2021-07-06 12:47:58 +0800

    Add files via upload

---
+++ 1-1 class 3.py
@@ -0,0 +1,17 @@
+eng= input ('eng score?')
+math= input ('math score?')
+
+eng=int(eng)
+math=int(math)
+
+if eng > 90 and math > 90:
+    print ("get money")
+    
+elif math < 90:
+    print ("no money")
+    
+elif eng < 90:
+    print ("no money")
+    
+
+    
